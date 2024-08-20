39dfa2440573674b55fd4fe3bc5d140bdcd46eed
Wato-Adano <watobuchaadano@gmail.com>
2024-08-20 21:24:12 +0300

    add files

--- api/views.py
+++ api/views.py
@@ -11,9 +11,7 @@ from models import ClassroomPeriod
 from models import Courses
 from models import Student
 from models import Class
-
-
-
+from teacher.models import Teacher
 
 class StudentListView(APIView):
     def get(self, request):
@@ -244,3 +242,103 @@ def delete(self,request,id):
         courses.delete()
         return Response(status=status.HTTP_202_ACCEPTED)
     
+
+
+class TeacherListView(APIView):
+    def get(self,request):
+        Teachers = Teacher.objects.all()
+        serializer = TeacherSerializer(Teachers, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = TeacherSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status= status.HTTP_201_CREATED)
+        
+        else:
+            return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST)
+    
+class StudentDetailView(APIView):
+    def get(self,request,id):
+        student = Student.objects.get(id=id)
+        serializer = StudentSerializer(student)
+        return Response(serializer.data)
+    
+    def put(self,request,id):
+        student = Student.objects.get(id=id)
+        serializer = StudentSerializer(student, data=request.data)
+        if serializer.is_valid():
+         serializer.save()
+         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+          return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self,request,id):
+        student = Student.objects.get(id=id)
+        student.delete()
+        return Response(status=status.HTTP_202_ACCEPTED)
+
+    def enroll(self, student, course_id):
+        course = Courses.objects.get(id = course_id)
+        student.courses.add(course)
+
+
+
+    def post(self, request,id):
+        student = Student.obj<ects.get(id=id)
+        action = request.data.get("action")
+        if action=="email":
+            course_id = request.data.get("course_id")
+            self.enroll(student, course_id)
+
+        return Response(status=status.HTTP_201_CREATED)
+
+
+
+
+
+class TeacherDetailView(APIView):
+    def get(self,request,id):
+        teachers = Teacher.objects.get(id=id)
+        serializer = TeacherSerializer(teachers)
+        return Response(serializer.data)
+    
+    def put(self,request,id):
+       teachers = Teacher.objects.get(id=id)
+       serializer = TeacherSerializer(teachers, data=request.data)
+       if serializer.is_valid():
+         serializer.save()
+         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+       else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    def delete(self,request,id):
+        teachers = Teacher.objects.get(id=id)
+        teachers.delete()
+        return Response(status=status.HTTP_202_ACCEPTED)
+
+    def assign_course(self, teacher, course_id):
+        course = Courses.objects.get(id=course_id)
+        teacher.courses.add(course)
+    def assign_class(self, teacher, class_id):
+        student_class = Student.objects.get(id=class_id)
+        student_class.teacher = teacher
+        student_class.save()
+    def post(self, request, id):
+        teacher = Teacher.objects.get(id=id)
+        action = request.data.get("action")
+        if action == "assign_course":
+            course_id = request.data.get("course_id")
+            self.assign_course(teacher, course_id)
+            return Response(status=status.HTTP_201_CREATED)
+        elif action == "assign_class":
+            class_id = request.data.get("class_id")
+            self.assign_class(teacher, class_id)
+            return Response(status=status.HTTP_201_CREATED)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
